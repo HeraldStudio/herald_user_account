4f1f316841c9dfd3a55674c3942c32a2ae7e0edf
Tairy <tairyguo@gmail.com>
2013-10-25 13:34:21 +0800

    ss

--- Lib/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'LoginAction.class.php';
-if(!empty($_POST['username']) && !empty($_POST['password'])){
-	$user = new LoginAction();
-	$user -> main();
-}else{
-	echo "用户名或密码为空";
-}
